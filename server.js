10beba3ddf2222670132732e580a40b530583ecd
malcolmberry96 <malcolm.berry65@gmail.com>
2018-12-23 12:20:27 -0600

    server complete

--- server.js
+++ server.js
@@ -43,12 +43,78 @@ app.get("/scrape",function(req, res){
 
             result.title = $(this)
                 .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+        //New article created using the result object built from scraping
+        db.Article.create(result)
+            .then(function(dbArticle){
+                //View the added result in the console 
+            console.log(dbArticle);
+            })
+            .catch(function(err){
+                //If any error occured, catch and log
+            console.log(err);
+            });
+        });
 
+        //Send message 
+        res.send("Scrape Complete");
+    });
+});
+
+//Route for grabbing the articles 
+app.get("/articles", function(req,res){
+    db.Article.find({})
+        .then(function(dbArticle){
+            //Send articles back to client
+            res.json(dbArticle);
         })
+        .catch(function(err){
+            //If an error occured send it to the client 
+            res.json(err);
+        });
+});
 
-
+//Route for grabbing a specific article by id, populate it 
+app.get("/articles/:id", function(req, res){
+    //Using the id passed in the id paramater, query that finds the matching one in the db
+    db.Article.findOne({_id: req.params.id })
+    //and populate all of the notes associated with it 
+    .populate("note")
+    .then(function(dbArticle){
+        //If able to successfully find an article with a good id, send it back to the client
+        res.json(dbArticle); 
     })
+    .catch(function(err){
+        res.json(err);
+    });
+});
 
+//Route for saving/updating an article's associated Note
+app.post("/articles/:id", function(req,res){
+    //Create a new note and pass it to the req.body entry 
+    db.Note.create(req.body)
+    .then(function(dbNote){
+        //If a note was created successfuly, find one article with an '_id' equal to 'req.params.id'
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+    return db.Article.findOneAndUpdate({_id: req.paramas.id }, {note: dbNote._id}, {new:true});
     })
-})
+    .then(function(dbArticle){
+        //If the article was successfully updated send it back to the client 
+        res.json(dbArticle);
+    })
+    .catch(function(err){
+        //If an error occured, send it to the client
+        res.json(err);
+    });
+});
+
+//Server start 
+app.listen(PORT, function(){
+    console.log("App running on port " + PORT + "!");
+});
+
 
