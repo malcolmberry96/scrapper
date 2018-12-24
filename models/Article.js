bbda0f3bb6a1dd70bd8be8083be2aede4e68d299
malcolmberry96 <malcolm.berry65@gmail.com>
2018-12-24 09:37:50 -0600

    app.js work

--- models/Article.js
+++ models/Article.js
@@ -1,3 +1,30 @@
 const mongoose = require("mongoose");
 
-const 
+//Save a reference for the schema constructor 
+const Schema = mongoose.Schema;
+
+//Using the schema constructor, create a new UserSchema object 
+const ArticleSchema = new Schema({
+    //required link and type of string
+    title: {
+        type: String,
+        required: True
+    },
+    //required link and type of string 
+    link: {
+        type: String, 
+        required: true
+    },
+    //note is an object that stores the not id 
+    //the ref property links the objectid to the not model 
+    //allows to populate with the associated note 
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+//Export the model
+module.exports = Article;
--- public/app.js
+++ public/app.js
@@ -1,1 +1,35 @@
-//require mongoose 
+
+$.getJSON("/article", function(data){
+    //Fore each article 
+    for (const i = 0; i < data.length; i++){
+        //Display the article on the page
+        $("#articles").append("<p data-id=" + data[i]._id + ">" + data[i].title + "<br />" + data[i].link + "</p>");
+    }
+});
+
+//Whenever someone clicks a p-tag
+$document.on("click", "p", function(){
+    //Empty the notes from the note section 
+    $("#notes").empty();
+    //Save the id from the p tag 
+    const thisId = $(this).attr("data-id");
+
+//Now make an ajax call for the Article 
+$.ajax({
+    method:"GET",
+    url: "/articles" + thisId
+})
+    //With that done, add the note information to the page 
+    .then(function(data){
+        console.log(data);
+        //The title of the article 
+        $("#notes").append("<h2>" + data.title + "<h2>")
+        //An input to enter a new title 
+        $("#notes").append("<input id='titleinput' name='title' >");
+        //A textarea to add a new note body 
+        $("#notes").append("<textarea id='bodyinput' name='body'></textarea");
+        //A button to submit a new text 
+        $("#notes").append("button data-id=' " + data._id + " 'id='savenote'>>")
+    })
+})
+
