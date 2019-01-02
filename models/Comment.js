349def24d20ed46665bed64bcbb5cc75b8803575
malcolmberry96 <malcolm.berry65@gmail.com>
2019-01-02 09:11:32 -0600

    navbar

--- models/Comment.js
+++ models/Comment.js
@@ -4,13 +4,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 //Using the schema constructor a note schema object is created 
-const NoteSchema = new Schema({
-    title: String,
-    body: String, 
+const CommentSchema = new Schema({
+    name: {
+        type: String
+    },
+    body: {
+        type: String,
+        required: true
+    }
 });
 
 //creates model using mongoose 
-const Note = mongoose.model("Note", NoteSchema);
+const Comment = mongoose.model("Comment", NoteSchema);
 
 //Export the Note model 
-module.export = Note;
+module.export = Comment;
--- models/index.js
+++
@@ -1,6 +0,0 @@
-//Exporting an object containing the models 
-
-module.exports = {
-    Article: require("./Article"),
-    Note: require("./Note")
-}
